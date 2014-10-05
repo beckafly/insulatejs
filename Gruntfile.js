6fb63f1117534cdcc2abe9a55208fa6e6264c664
Alex Becker <alebecke@cisco.com>
2014-10-04 23:13:50 -0700

    grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,25 @@
 module.exports = function(grunt) {
-       'use strict';
-       // Project configuration.
-       grunt.initConfig({
-               jasmine : {
-                       src : 'insulate.js',
-                       options : {
-                               specs : 'test/SpecInsulate.js'
-                       }
-               }
-       });
-       grunt.loadNpmTasks('grunt-contrib-jasmine');
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        jasmine: {
+            src: 'insulate.js',
+            options: {
+                specs: 'test/SpecInsulate.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! insulate.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'insulate.js',
+                dest: 'insulate.min.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['jasmine', 'uglify']);
 };
