c147d7f9aee14827d064fce48c430e1fc519dc31
Alex Becker <byaxy@insulate.com>
2014-10-06 14:51:25 -0700

    added older browser compat and surcomventing Object.defineProperty overrite attempt

--- insulate.js
+++ insulate.js
@@ -13,56 +13,90 @@
 
 
 
-	(function() {
-		
+	(function(window, undefined) {
+
 	    var cf = {};
 	    var validName = /^[$A-Z_][0-9A-Z_$]*$/i;
 	    var console = window.console;
-	 // to turn on error massages in console set insulate.logErrors = true; somwere in your code
-	 
-	 function logErr(error) {
-	 	if (insulate.log){
-	 		console.warn(error);
-	 	} else {
-	 		return;
-	 	}
-	}
+	    var log;
+	    var odp = clone(Object.defineProperty || null); // call function to clone Object.defineProperty in case it's fucked with later
+
+	    // clone Object.defineProperty
+	    function clone(obj) {
+	        if (obj === null || typeof(obj) !== "object"){
+	            return obj;
+	        }
+
+	        var temp = obj.constructor(); // changed
+
+	        for (var key in obj) {
+	            if (obj.hasOwnProperty(key)) {
+	                temp[key] = clone(obj[key]);
+	            }
+	        }
+	        return temp;
+	    }
+	    // cf.Object.defineProperty = ;
+
+	    // to turn on error massages in console set insulate.logErrors = true; somwere in your code
+
+	    function logErr(error) {
+	        if (log) {
+	            console.warn(error);
+	        } else {
+	            return;
+	        }
+	    }
 	    // main function creates objects
+
+
 	    function createImtble(name, context) {
-	    	var cnt;
+
+
+	        var cnt;
 	        if (typeof context === "undefined") {
 	            cnt = window;
 	        } else {
 	            cnt = context;
 	        }
 
-	        var localMYOBJ;
-	        try{
-		        Object.defineProperty(cnt, name, {
+	        
+	        if (typeof odp !== null) {
 
-		            get: function() {
-		                return localMYOBJ;
-		            },
-		            set: function(val) { 
-		            
-			            	if (cnt[name]){
-			            		logErr("This proprty has already been set and will not be changed");
-			            		return null;
-			            	} 
-			               	localMYOBJ = val; 
-		       			 
-		            }
-		        });
-		   }catch(err){
-		   	logErr(err.message);
-		   } finally {
-		   	return;
-		   }
+	            var localMYOBJ;
+	            try {
+	                odp(cnt, name, {
+
+	                    get: function() {
+	                        return localMYOBJ;
+	                    },
+	                    set: function(val) {
+
+	                        if (cnt[name]) {
+	                            logErr("This proprty has already been set and will not be changed");
+	                            return null;
+	                        }
+	                        localMYOBJ = val;
+
+	                    }
+	                });
+	            } catch (err) {
+	                logErr(err.message);
+	            } finally {
+	                return;
+	            }
+	        } else {
+	            logErr("older browser, will not be secure");
+	            cnt[name] = undefined;
+
+	        }
+
+
 	    }
 
 
 
-		/****************
+	    /****************
 		insulate.createConstant() will create a constant in a global namespace. Needs a name and the data to set the constant
 		
 		example 
@@ -71,31 +105,30 @@
 		example
 		insulate.createConstant("MYCONST"); MYCONST = "hello world".
 		***************/
-	    
+
 	    cf.createConstant = function(name, contents) {
-		    try{
-		    	if(window[name] && typeof Object.getOwnPropertyDescriptor(window, name).set !== "function") {
-		    		throw "This variable has been decleard without insulate, please pick another name";
-		    	} else if (typeof name !== "string") {
-		            throw "First argument needs to be constant name";
-		        } else if (!name.match(validName)) {
-		            throw "Name needs to be a valid javascript name";
-		        } else {
-		            createImtble(name);
-		            if (contents) {
-		            window[name] = contents;
-		        	}
-		        }
-		    }catch(err){
-		    	logErr(err);
-		    }
-		    finally {
-		    	return;
-		    }
+	        try {
+	            if (window[name] && typeof Object.getOwnPropertyDescriptor(window, name).set !== "function") {
+	                throw "This variable has been decleard without insulate, please pick another name";
+	            } else if (typeof name !== "string") {
+	                throw "First argument needs to be constant name";
+	            } else if (!name.match(validName)) {
+	                throw "Name needs to be a valid javascript name";
+	            } else {
+	                createImtble(name);
+	                if (contents) {
+	                    window[name] = contents;
+	                }
+	            }
+	        } catch (err) {
+	            logErr(err);
+	        } finally {
+	            return;
+	        }
 	    };
 
-		
-		/***************
+
+	    /***************
 		insulate.createObject() will create an object in global space or in any context if supplied
 		needs a name and optianoly context (an object). Nested objects can be passed with dotted notaition - obj1.obj2.obj3 or as a named refference
 
@@ -109,22 +142,22 @@
 	        //console.log(context);
 	        context = context || window;
 	        try {
-		        if (typeof name !== "string") {
-		            throw "First argument needs to be object name";
-		        } else if (!name.match(validName)) {
-		            throw "Name needs to be a valid javascript name";
-		        } else {
-		            createImtble(name, context);
-		            context[name] = {};
-		        }
-		    } catch(err)  {
-		    	logErr(err);
-		    } finally {
-		    	return;
-		    }
+	            if (typeof name !== "string") {
+	                throw "First argument needs to be object name";
+	            } else if (!name.match(validName)) {
+	                throw "Name needs to be a valid javascript name";
+	            } else {
+	                createImtble(name, context);
+	                context[name] = {};
+	            }
+	        } catch (err) {
+	            logErr(err);
+	        } finally {
+	            return;
+	        }
 	    };
 
-	   
+
 	    /**************
 	    insulate.createMethod() will create a immutable  method (if created in an object made with insulate utility) 
 		needs a name, object and a function.
@@ -145,30 +178,30 @@
 
 	    cf.createMethod = function(name, context, contents) {
 
-	       try{
-		        if (typeof context !== "object") {
-		            throw "Second argument needs to be an object";
-		        } else if (contents && typeof contents !== "function") {
-		            throw "Third argument needs to be a function";
-		        } else if (typeof name !== "string") {
-		            throw "First argument needs to be Method name";
-		        } else if (!name.match(validName)) {
-		            throw "Name needs to be a valid javascript name";
-		        } else {
-		            createImtble(name, context);
-		            if(contents){
-		            	context[name] = contents;
-		            }
-		        }
-	   	} catch(err) {
-	   		logErr(err);
-	   	} finally {
-	   		return;
-	   	}
+	        try {
+	            if (typeof context !== "object") {
+	                throw "Second argument needs to be an object";
+	            } else if (contents && typeof contents !== "function") {
+	                throw "Third argument needs to be a function";
+	            } else if (typeof name !== "string") {
+	                throw "First argument needs to be Method name";
+	            } else if (!name.match(validName)) {
+	                throw "Name needs to be a valid javascript name";
+	            } else {
+	                createImtble(name, context);
+	                if (contents) {
+	                    context[name] = contents;
+	                }
+	            }
+	        } catch (err) {
+	            logErr(err);
+	        } finally {
+	            return;
+	        }
 
 	    };
 
-		/*************
+	    /*************
 		insulate.createMember() will create an imutable member in an object (created with insulate.createObject)
 		needs a name, object and data of any type. 
 		nested objects can be passed with dotted notaition - obj1.obj2.obj3 or as a named refference
@@ -187,54 +220,58 @@
 
 	    cf.createMember = function(name, context, contents) {
 
-	    	try {
-		        if (typeof name !== "string") {
-		            throw "First argument needs to be member name";
-		        } else if (typeof context !== "object") {
-		            throw "Second argument needs to be an object";
-		        } else if (!name.match(validName)) {
-		            throw "Name needs to be a valid javascript name";
-		        } else if (typeof contents === "function") {
-		            throw "Member can not be a function";
-		        } else {
-		            createImtble(name, context);
-		            if (contents){
-		            context[name] = contents;
-		        	}
-		        }
-		    } catch(err){
-		    	logErr(err);
-		    } finally {
-		    	return;
-		    }
+	        try {
+	            if (typeof name !== "string") {
+	                throw "First argument needs to be member name";
+	            } else if (typeof context !== "object") {
+	                throw "Second argument needs to be an object";
+	            } else if (!name.match(validName)) {
+	                throw "Name needs to be a valid javascript name";
+	            } else if (typeof contents === "function") {
+	                throw "Member can not be a function";
+	            } else {
+	                createImtble(name, context);
+	                if (contents) {
+	                    context[name] = contents;
+	                }
+	            }
+	        } catch (err) {
+	            logErr(err);
+	        } finally {
+	            return;
+	        }
 	    };
 
-	    cf.logErrors = function(){
-	    	insulate.log = true;
+	    log = false;
+
+	    cf.logErrors = function() {
+	        log = true;
 	    };
 
-	    cf.muteErrors = function(){
-	    	insulate.log = false;
+	    cf.muteErrors = function() {
+	        log = false;
 	    };
 
+
+
+	    // create inutable insulate onject and methods
 
 	    createImtble("insulate", window);
-	   
-
-	    var insulate=window.insulate = {};
-
+	    var insulate = window.insulate = {};
 	    createImtble("createConstant", insulate);
 	    createImtble("createObject", insulate);
 	    createImtble("createMethod", insulate);
 	    createImtble("createMember", insulate);
+	    createImtble("logErrors", insulate);
+	    createImtble("muteErrors", insulate);
 
-	    insulate.createConstant = cf.createConstant;
-	    insulate.createObject = cf.createObject;
-	    insulate.createMethod = cf.createMethod;
-	    insulate.createMember = cf.createMember;
-	    insulate.logErrors = cf.logErrors;
-	    insulate.log = false;
+	    // set insulate methods
+
+	    for (var mtd in cf) {
+	        if (cf.hasOwnProperty(mtd)) {
+	            insulate[mtd] = cf[mtd];
+	        }
+	    }
 
 
-	    
-	}());
+	}(window));
